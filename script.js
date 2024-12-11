c14c137e02bd89c9e2ab5024a65fda14ac9ce46e
Rajesh <rkraja730@gmail.com>
2024-12-12 01:35:49 +0530

    project add api

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+let data;
+const item_name = document.querySelector(".item-name");
+const item_image = document.querySelector(".item-image");
+const item_price = document.querySelector(".item-price");
+const item_quantity = document.querySelector(".item-count");
+const item_subtotal = document.querySelector(".item-subtotal");
+//fetching the details
+const fetchProductDetails = async () => {
+  try {
+    const response = await fetch(
+      "https://cdn.shopify.com/s/files/1/0883/2188/4479/files/apiCartData.json?v=1728384889"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    data = await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+(async () => {
+  await fetchProductDetails();
+  let image = document.createElement("img");
+  image.src = data.items[0].featured_image.url;
+  item_image.appendChild(image);
+  item_name.textContent = data.items[0].product_title;
+  item_price.textContent = 'Rs.'+data.items[0].presentment_price;
+  item_quantity.textContent = data.items[0].quantity;
+  item_subtotal.textContent='Rs.'+(data.items[0].quantity*data.items[0].presentment_price)
+})();
